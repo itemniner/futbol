57227f4e76f20fe0a48858d88913d7c8ff7a42f4
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-30 17:26:23 -0600

    Add appropriate helper methods from GamesCollection to Uniqable
    Co-Authored-By: Phil DeLong <philjdelong@users.noreply.github.com>
    Co-Authored-By: Ray <itemniner@users.noreply.github.com>
    Co-Authored-By: Sebastian Sloan <sasloan@users.noreply.github.com>

--- module/uniqable.rb
+++ module/uniqable.rb
@@ -21,4 +21,38 @@ module Uniqable
   def unique_seasons
     @games.map {|game| game.season}.uniq
   end
+
+  def all_team_ids
+    every_unique("team_id", @games_teams)
+  end
+
+  def opponents_team_id(team_id)
+    every_unique("team_id", all_opponent_games(team_id))
+  end
+
+  def home_teams
+    every_unique("home_team_id", @games)
+  end
+
+  def away_teams
+    every_unique("away_team_id", @games)
+  end
+
+  def team_seasons(team_id)
+    games_with_team(team_id).map do |game|
+      game.season
+    end.uniq
+  end
+
+  def team_opponents(team_id)
+    games_with_team(team_id).map do |game|
+      team_id == game.away_team_id ? game.home_team_id : game.away_team_id
+    end.uniq
+  end
+
+  def all_unique_teams_in_season(season)
+    home_ids = every_unique("home_team_id", all_games_in_season(season))
+    away_ids = every_unique("away_team_id", all_games_in_season(season))
+    (home_ids + away_ids).uniq
+  end
 end
