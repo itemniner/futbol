7dd25551f57f6bbb0a6ae0a6b8da40f85cb44a65
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-30 17:26:42 -0600

    Add appropriate helper methods from GamesCollection to Totalable
    Co-Authored-By: Phil DeLong <philjdelong@users.noreply.github.com>
    Co-Authored-By: Ray <itemniner@users.noreply.github.com>
    Co-Authored-By: Sebastian Sloan <sasloan@users.noreply.github.com>

--- module/totalable.rb
+++ module/totalable.rb
@@ -3,67 +3,35 @@ module Totalable
     games.sum { |game| goals(game) }
   end
 
-  def total_goals_of_team(team_id)
-    list_of_games_of_team(team_id).sum { |game_team| game_team.goals.to_i }
-  end
-
-  def total_goals_of_opponents(team_id)
-    all_opponent_games(team_id).sum { |game_team| game_team.goals.to_i }
-  end
-
-  def total_home_games(team)
+  def total_home_games_of_team(team)
     all_home_games_of_team(team).count
   end
 
-  def total_home_games
-    @games_teams.count do |game_team|
-      game_team.hoa == 'home'
-    end
-  end
-
-  def total_home_goals(team)
+  def total_home_goals_of_team(team)
     all_home_games_of_team(team).sum { |game| game.home_goals.to_i }
   end
 
-  def total_away_games_team(team)
+  def total_away_games_of_team(team)
     all_away_games_of_team(team).count
   end
 
-  def total_away_games
-    @games_teams.count do |game_team|
-      game_team.hoa == 'away'
-    end
-  end
-
-  def total_away_goals(team)
+  def total_away_goals_of_team(team)
     all_away_games_of_team(team).sum { |game| game.away_goals.to_i }
   end
 
   def total_away_wins_in_season(team_id, season)
     away_games_in_season(team_id, season).count do |game|
-      away_win?(game)
-    end
-  end
-
-  def total_away_wins
-    @games_teams.count do |game_team|
-      game_team.hoa == 'away' && game_team.result == 'WIN'
+      game.away_win?
     end
   end
 
   def total_home_wins_in_season(team_id, season)
     home_games_in_season(team_id, season).count do |game|
-      home_win?(game)
+      game.home_win?
     end
   end
 
-  def total_home_wins
-    @games_teams.count do |game_team|
-      game_team.hoa == 'home' && game_team.result == 'WIN'
-    end
-  end
-
-  def total_team_wins(team_id, season)
+  def total_team_wins_in_season(team_id, season)
     total_home_wins_in_season(team_id, season) + total_away_wins_in_season(team_id, season)
   end
 
@@ -86,12 +54,12 @@ module Totalable
   end
 
   def total_opponent_goals(team_id)
-    find_opponents_goals_if_home_team(team_id) + find_opponents_goals_if_away_team(team_id)
+    total_opponents_goals_if_home_team(team_id) + total_opponents_goals_if_away_team(team_id)
   end
 
   def total_wins_across_seasons(team_id)
     unique_seasons.sum do |season|
-      games_with_team_in_season(team_id, season) != nil ? total_team_wins(team_id, season) : 0
+      games_with_team_in_season(team_id, season) != nil ? total_team_wins_in_season(team_id, season) : 0
     end
   end
 
@@ -103,12 +71,36 @@ module Totalable
 
   def total_wins_against(team_id, team_opponent)
     games_between(team_id, team_opponent).count do |game|
-      team_id == game.home_team_id ? home_win?(game) : away_win?(game)
+      team_id == game.home_team_id ? game.home_win? : game.away_win?
     end
   end
 
   def total_games_between(team_id, team_opponent)
     games_between(team_id, team_opponent).length
+  end
+
+  def total_home_games
+    @games_teams.count do |game_team|
+      game_team.hoa == 'home'
+    end
+  end
+
+  def total_home_wins
+    @games_teams.count do |game_team|
+      game_team.hoa == 'home' && game_team.result == 'WIN'
+    end
+  end
+
+  def total_away_games
+    @games_teams.count do |game_team|
+      game_team.hoa == 'away'
+    end
+  end
+
+  def total_away_wins
+    @games_teams.count do |game_team|
+      game_team.hoa == 'away' && game_team.result == 'WIN'
+    end
   end
 
   def total_ties
@@ -126,13 +118,20 @@ module Totalable
   end
 
   def total_wins_of_team(team_id)
-    games_with_team(team_id).count { |game_team| game_team.result == "WIN" }
+    games_teams_with_team(team_id).count { |game_team| game_team.result == "WIN" }
   end
 
-
-  def total_shots_taken_by_team(team_id)
-    list_of_games_of_team(team_id).sum { |game_team| game_team.shots.to_i }
+  def total_goals_of_team(team_id)
+    list_of_games_of_team(team_id).sum { |game_team| game_team.goals.to_i }
   end
+
+  def total_goals_of_opponents(team_id)
+    all_opponent_games(team_id).sum { |game_team| game_team.goals.to_i }
+  end
+
+  # def total_shots_taken_by_team(team_id)
+  #   list_of_games_of_team(team_id).sum { |game_team| game_team.shots.to_i }
+  # end
 
   def total_wins_of_coach_in_season(coach, game_ids)
     all_coach_games_in_season(coach, game_ids).count do |game|
@@ -147,6 +146,67 @@ module Totalable
   def total_team_tackles_in_season(team_id, game_ids)
     all_games_of_team_in_season(team_id, game_ids).sum do |game|
       game.tackles.to_i
+    end
+  end
+
+  def number_of_wins
+    winner_goals = []
+    @games_teams.each do |game_team|
+      if game_team.result == "WIN"
+        winner_goals << game_team.goals.to_i
+      end
+    end
+    winner_goals
+  end
+
+  def number_of_losses
+    losser_goals = []
+    @games_teams.each do |game_team|
+      if game_team.result == "LOSS"
+        losser_goals << game_team.goals.to_i
+      end
+    end
+    losser_goals
+  end
+
+  def number_of_wins_in(collection)
+    collection.count { |game_team| game_team.result == "WIN" }
+  end
+
+  def total_shots_taken_by_team_in_season(team_id, game_ids)
+    all_games_of_team_in_season(team_id, game_ids).sum { |game_team| game_team.shots.to_i }
+  end
+
+  def total_goals_of_team_in_season(team_id, game_ids)
+    all_games_of_team_in_season(team_id, game_ids).sum { |game_team| game_team.goals.to_i }
+  end
+
+  def number_of_games_in_each_season
+    seasons_of_games = @games.group_by {|game| game.season}
+    seasons_of_games.values.map {|value| value.length}
+  end
+
+  def team_goals(team_id)
+    all_opponent_games(team_id).map { |game_team| game_team.goals.to_i }
+  end
+
+  def opponents_goals(team_id)
+    list_of_games_of_team(team_id).map { |team_game| team_game.goals.to_i }
+  end
+
+  def goals(game)
+    game.home_goals.to_i + game.away_goals.to_i
+  end
+
+  def total_opponents_goals_if_away_team(team_id)
+    games_with_team(team_id).sum do |game_team|
+      team_id == game_team.away_team_id ? game_team.home_goals.to_i : 0
+    end
+  end
+
+  def total_opponents_goals_if_home_team(team_id)
+    games_with_team(team_id).sum do |game_team|
+      team_id == game_team.home_team_id ? game_team.away_goals.to_i : 0
     end
   end
 end
